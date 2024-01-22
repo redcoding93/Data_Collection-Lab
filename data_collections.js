a339b8a9705eeac65cb0a33f9de048414cfd5249
Vanessa Ingram <vingram15@gmail.com>
2024-01-21 22:58:07 -0800

    Update data_collections.js

--- data_collections.js
+++ data_collections.js
@@ -1,6 +1,5 @@
-//Part 1: Refactoring Old Code
-let str =
-  "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor's Assistant,26";
+//Part 1: Refactoring old code
+let str = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz,Master,58\n98,Bill,Doctor's Assistant,26";
 
 let placeholder = '';
 let cell1 = '';
@@ -10,7 +9,9 @@ let cell4 = '';
 let char = '';
 let twoDimensionalArray = [];
 
+// Loop for each character
 for (char of str) {
+    //Switch case to determin what to do with each char
   switch (char) {
     case ',':
       if (!cell1) {
@@ -36,44 +37,34 @@ for (char of str) {
       cell4 = '';
       break;
     default:
-      placeholder += char.toLowerCase();
+      placeholder += char;
   }
 }
-//Part 2: Expanding Functionality
+
+//Part 2: expanding functionality
 //Two dimensional array
-if (cell1 || cell2 || cell3 || cell4) {
-  twoDimensionalArray.push([cell1, cell2, cell3, cell4]);
+if (cell1 || cell2 || cell3 || cell4){
+	twoDimensionalArray.push([cell1, cell2, cell3, cell4]);
 }
 console.log(twoDimensionalArray);
 
-//Part 3: Transforming Data
-//Array.map(): The map() method in JavaScript creates a new array by applying a provided function to each element of the original array. 
-//It allows us to transform the elements of an array based on specific logic.
+//Part 3: Transforming data
+let[keys, ...rows] = twoDimensionalArray;
+let result = rows.map(r => (keys.reduce((o,k,i) => (o[k] = r[i], o), {})));
+//uses map method to iterate over reach row in the rows array
 
-//Array.reduce(): The reduce() method in JavaScript applies a function against an accumulator and each element in the array (from left to right) to reduce it to a single value. 
-//It is commonly used to perform calculations or transformations on arrays.
-
-let [keys, ...rows] = twoDimensionalArray;
-//Initializes the array variable with an array of arrays. The let keyword is used to declare the variable, and the array is assigned using square brackets.
-
-//The reduce() method is used to transform the row into an object with key-value pairs.
-let result = rows.map(r => (keys.reduce((o, k, i) => (o[k] = r[i], o), {})));
-//Uses the map() method to iterate over each row in the rows array.
-
-//Logs result
+//log result
 console.log(result);
 
-//Part 4: Sorting and Manipulating Data
+//Part 4: Sorting and manipulating data
+//remove last element from sorted array
+result.pop()
+console.log(result);
 
-//Remove the last element from the sorted array.
-result.pop();
-// console.log(result);
+//Insert the following object at index 1
+result.splice(1,0,{ id: "48", name: "Barry", occupation: "Runner", age: "25" });
 
-//Insert the following object at index 1:
-result.splice(1, 0, { id: "48", name: "Barry", occupation: "Runner", age: "25" });
-// console.log(result);
-
-//Add the following object to the end of the array:
+//add the following object to the end of the array:
 result.push({ id: "7", name: "Bilbo", occupation: "None", age: "111" });
 console.log(result);
 
@@ -81,16 +72,29 @@ console.log(result);
 let total = 0;
 for(let i = 0; i < twoDimensionalArray.length; i++){
 	total += twoDimensionalArray[i].age;
-  	
-  }	
+}
 	let average = total / twoDimensionalArray.length;
 	console.log(average);
 
-// Part 5: Full Circle
-// The map() method on the twoDimensionalArray
+//Part 5: Full Circle
 let reversedString = twoDimensionalArray.map(row => row.join(',').concat('\n'));
-//join was used to convert the object arrays into strings and concat was used to inlcude the \n character at the end of each row like the original string.
 console.log(reversedString);
-// toString() was used to turn the array holding the string into a stand alone string.
+//uses toString() to convert the array containing a string, into a stand alone string
 let convert = reversedString.toString();
 console.log(convert);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+			
